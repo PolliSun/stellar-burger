b4e5cac30d7cffae82ba2d76fd6ecd02617a053b
Polina Lepekhina <wdvrgnyjio6@gmail.com>
2024-07-17 02:46:42 +0300

    описан тест rootReducer

---
+++ src/services/slices/__tests__/rootReducer.test.ts
@@ -0,0 +1,14 @@
+import {rootReducer} from '../../rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { expect, test } from '@jest/globals';
+
+test('Тест проверка инициализации rootReducer', () => {
+    const action = { type: 'UNKNOWN_ACTION' };
+    const store = configureStore({
+        reducer:rootReducer
+    });
+
+    const state = rootReducer(undefined, action);
+    expect(state).toEqual(store.getState());
+});
